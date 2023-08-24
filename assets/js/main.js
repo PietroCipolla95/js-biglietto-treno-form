c5fc170c699f7e025aa4fa071c9b0fa813f0fbec
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-24 16:38:30 +0200

    add some css

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,35 +9,26 @@ submit.addEventListener('click', function() {
     let price = (km * 0.21)
     console.log(price);
 
+    let ageRange = document.getElementById('age').value;
+    console.log(ageRange);
+
+    if (ageRange === 'Minorenne') {
+
+        price = Number(price * 0.8)
+        console.log(price.toFixed(2));
+    
+    } else if (ageRange === 'Over_65') {
+        
+        price = Number(price * 0.6)
+        console.log(price.toFixed(2));
+        
+    }  else if (ageRange === 'no_sale') {
+
+        console.log(price.toFixed(2));
+
+    }
+
 })
-
-
-
-
-
-
-/* 
-let price = (km * 0.21)
-console.log(price);
-document.getElementById('price').innerHTML = price + ' €'
-
-
-if (age < 18) {
-
-    price = Number(price * 0.8)
-    document.getElementById('sales').innerHTML = (price.toFixed(2) + ' €')
-
-} else if (age >= 65) {
-    
-    price = Number(price * 0.6)
-    document.getElementById('sales').innerHTML = (price.toFixed(2) + ' €')
-    
-}  
- */
-
-
-
-
 
 
 
