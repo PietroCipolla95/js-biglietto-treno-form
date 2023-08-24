9f42a294e24f3a081870c04311d89babf1206f2f
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-24 15:55:07 +0200

    first const add

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+const submit = document.querySelector('button')
+
+submit.addEventListener('click', function() {
+
+    let userName = document.getElementById('name').value;
+    console.log(userName);
+
+})
+
+
+
+
+
+
+/* 
+let price = (km * 0.21)
+console.log(price);
+document.getElementById('price').innerHTML = price + ' €'
+
+
+if (age < 18) {
+
+    price = Number(price * 0.8)
+    document.getElementById('sales').innerHTML = (price.toFixed(2) + ' €')
+
+} else if (age >= 65) {
+    
+    price = Number(price * 0.6)
+    document.getElementById('sales').innerHTML = (price.toFixed(2) + ' €')
+    
+}  
+ */
+
+
+
+
+
+
+
