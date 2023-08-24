cd6511f7f180ccecb5cbb6bda96ca21938b7c98e
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-24 17:34:09 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ submit.addEventListener('click', function() {
     let userName = document.getElementById('name').value;
     let km = document.getElementById('km_todo').value;
     console.log(userName, km);
+    document.getElementById('passenger_name').innerHTML = userName
 
     let price = (km * 0.21)
     console.log(price);
@@ -16,17 +17,30 @@ submit.addEventListener('click', function() {
 
         price = Number(price * 0.8)
         console.log(price.toFixed(2));
+        document.getElementById('cost').innerHTML = price.toFixed(2) + '€'
+        document.getElementById('offer').innerHTML = 'Sconto del 20%'
     
     } else if (ageRange === 'Over_65') {
         
         price = Number(price * 0.6)
         console.log(price.toFixed(2));
+        document.getElementById('cost').innerHTML = price.toFixed(2) + '€'
+        document.getElementById('offer').innerHTML = 'Sconto del 40%'
         
     }  else if (ageRange === 'no_sale') {
 
         console.log(price.toFixed(2));
+        document.getElementById('cost').innerHTML = price.toFixed(2) + '€'
+        document.getElementById('offer').innerHTML = 'Biglietto Standard'
 
     }
+
+    let wagonDom = Math.ceil(Math.random() * 30) + 1;
+    document.getElementById('wagon').innerHTML = wagonDom;
+    
+    
+    let codeDom = Math.ceil(Math.random() * 50000);
+    document.getElementById('code').innerHTML = codeDom;
 
 })
 
